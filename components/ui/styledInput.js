f81f7cdf16dc0cc7be02879c449396866b35329b
Shakil Hossain <hshakilst@gmail.com>
2021-02-03 11:27:27 +0600

    Fixed Input Field Issue

--- components/ui/styledInput.js
+++ components/ui/styledInput.js
@@ -5,56 +5,77 @@ import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  root: {
-    "& .MuiOutlinedInput-root": {
-      marginRight: -8,
-      marginLeft: 8,
-      fontSize: "1rem",
-      fontWeight: "400",
-      letterSpacing: "0.047rem",
-      marginBottom: 0,
-    },
-    "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-      borderStyle: "none",
-      marginBottom: -2,
-    },
-    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-      borderStyle: "none",
-    },
-    "& .MuiInputLabel-outlined": {
-      marginLeft: 8,
-      fontSize: "1rem",
-      fontWeight: "400",
-      letterSpacing: "0.047rem",
-    },
-    "& .MuiInputLabel-outlined.Mui-focused": {
-      color: "black",
-    },
-    "& .MuiGrid-root.MuiGrid-container": {
-      borderRadius: "1rem",
-      paddingTop: 8,
-      paddingLeft: 12,
-      paddingRight: 12,
-      paddingBottom: 4,
-      backgroundColor: "#EFF0F6",
-      margin: "auto",
-    },
-    "& .MuiIconButton-root": {
-      paddingLeft: 0,
-      paddingRight: 0,
-      color: "black",
-      marginBottom: 4,
-      backgroundColor: "#EFF0F6",
-    },
-    "& .MuiSvgIcon-fontSizeSmall": {
-      fontSize: "1rem",
-    },
-  },
-});
-
 const StyledInput = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& .MuiOutlinedInput-root": {
+        marginRight: -8,
+        marginLeft: 8,
+        fontSize: "1rem",
+        fontWeight: "400",
+        letterSpacing: "0.047rem",
+        marginBottom: 0,
+      },
+      "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+        borderStyle: "none",
+        marginBottom: -2,
+      },
+      // "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      //   borderColor: "red"
+      // },
+      "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+        // borderColor: "transparent",
+        borderStyle: "none",
+      },
+      // "& .MuiOutlinedInput-input": {
+      //   color: "green"
+      // },
+      // "&:hover .MuiOutlinedInput-input": {
+      //   color: "red"
+      // },
+      // "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+      //   color: "purple"
+      // },
+      "& .MuiInputLabel-outlined": {
+        marginLeft: 8,
+        fontSize: "1rem",
+        fontWeight: "400",
+        letterSpacing: "0.047rem",
+      },
+      // "&:hover .MuiInputLabel-outlined": {
+      //   color: "red"
+      // },
+      "& .MuiInputLabel-outlined.Mui-focused": {
+        color: props.error ? "#C30052" : "#14142b",
+      },
+      "& .MuiGrid-root.MuiGrid-container": {
+        borderRadius: "1rem",
+        paddingTop: 8,
+        paddingLeft: 12,
+        paddingRight: 12,
+        paddingBottom: 4,
+        backgroundColor: props.error ? "#FFDFED" : "#EFF0F6",
+        margin: "auto",
+        // border: `${props.error && "0.125rem solid #ED2E7E"}`,
+      },
+      "& .MuiIconButton-root": {
+        paddingLeft: 0,
+        paddingRight: 0,
+        color: "black",
+        marginBottom: 4,
+        backgroundColor: "transparent",
+      },
+      "& .MuiSvgIcon-fontSizeSmall": {
+        fontSize: "1rem",
+      },
+      "& .Mui-error": {
+        transitions: theme.transitions.create(["border"]),
+      },
+    },
+  }));
+
   const classes = useStyles();
+
   return (
     <Container className={classes.root} xs="sm" style={props.style}>
       <Grid container alignItems={"center"}>
@@ -63,18 +84,18 @@ const StyledInput = (props) => {
         </Grid>
         <Grid item xs={10}>
           <TextField
+            name={props.name}
             onClick={props.onClick}
             label={props.label}
             variant="outlined"
             type={props.type}
             size="small"
-            style={props.style}
+            error={props.error}
+            inputRef={props.inputRef}
           />
         </Grid>
         <Grid item xs={1}>
-          <IconButton variant="contained" component="icon">
-            {props.endIcon}
-          </IconButton>
+          <IconButton>{props.endIcon}</IconButton>
         </Grid>
       </Grid>
     </Container>
