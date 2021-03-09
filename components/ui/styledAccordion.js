179b37a11407176d626c3ec7f2cb9f95c76921fd
shagoto-bhakat <shagotobhakat1996@gmail.com>
2021-03-09 16:56:36 +0600

    add link into styledAccordion.js

--- components/ui/styledAccordion.js
+++ components/ui/styledAccordion.js
@@ -6,6 +6,7 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import StyledButton from "./styledButton";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Link from "next/link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,6 +37,10 @@ const useStyles = makeStyles((theme) => ({
       position: "absolute",
       top: 10,
       right: 50,
+    },
+    "& .MuiTypography-body1": {
+      margin: "auto",
+      textAlign: "center",
     },
   },
 }));
@@ -70,17 +75,86 @@ export default function SimpleAccordion(props) {
         <AccordionDetails>
           <Typography
             style={{
+              width: "100%",
               background: "#EFF0F6",
               borderRadius: "1rem",
               color: "#14142B",
               fontSize: "1rem",
               fontWeight: 400,
               letterSpacing: "0.047rem",
-              padding: "1rem",
+              paddingTop: "1rem",
+              paddingBottom: "1rem",
+              paddingRight: "1rem",
+              paddingLeft: "1rem",
             }}
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            <ul>
+              <li>
+                <Link href="/dashboard/inventory/items">
+                  <a
+                    style={{
+                      color: "#14142B",
+                      fontSize: "1rem",
+                      fontWeight: 400,
+                    }}
+                  >
+                    Items
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/dashboard/inventory/suppliers">
+                  <a
+                    style={{
+                      color: "#14142B",
+                      fontSize: "1rem",
+                      fontWeight: 400,
+                    }}
+                  >
+                    Suppliers
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/dashboard/inventory/warehouses">
+                  <a
+                    style={{
+                      color: "#14142B",
+                      fontSize: "1rem",
+                      fontWeight: 400,
+                    }}
+                  >
+                    Warehouses
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/dashboard/inventory/purchase-orders">
+                  <a
+                    style={{
+                      color: "#14142B",
+                      fontSize: "1rem",
+                      fontWeight: 400,
+                    }}
+                  >
+                    Purchases
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/dashboard/inventory/store-requisitions">
+                  <a
+                    style={{
+                      color: "#14142B",
+                      fontSize: "1rem",
+                      fontWeight: 400,
+                    }}
+                  >
+                    Requisitions
+                  </a>
+                </Link>
+              </li>
+            </ul>
           </Typography>
         </AccordionDetails>
       </Accordion>
