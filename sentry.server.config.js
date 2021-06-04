6f27eeab52e5bc1bd4b1d2b56e4ff0b5834b64f9
Shagoto Bhakat <shagotobhakat1996@gmail.com>
2021-06-05 04:12:56 +0600

    Sentry Fix 5

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -13,4 +13,6 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
+  tracesSampleRate: 1.0,
+  environment: "production",
 });
